d95340695a911ac908754a1551a962ad34e39b3d
Ryan Aznar <raznar@ncsu.edu>
2019-12-03 13:32:58 -0500

    Added wire serialization to coordinates

--- 3_Day/Day3.py
+++ 3_Day/Day3.py
@@ -1,9 +1,56 @@
+from copy import deepcopy
 
-
-with open("Day3Input.txt", "r") as input:
+with open("TestInput.txt", "r") as input:
 	wire1 = [wire.strip() for wire in input.readline().split(",")]
 	wire2 = [wire.strip() for wire in input.readline().split(",")]
 
-print(wire1)
-print(wire2)
 
+def directionToCoord(wire):
+	coordList = []
+	origin = {"x": 0, "y": 0}
+	coordList.append(origin)
+
+	currentPos = {"x":0, "y":0}
+
+	for line in wire:
+		direction = line[0]
+
+		magnitude = int(line[1:len(line)])
+
+		if direction == "R":
+			for point in range(0, magnitude):
+				currentPos["x"] += 1
+				tempCoord = deepcopy(currentPos)
+				coordList.append(tempCoord)
+
+		elif direction == "U":
+			for point in range(0, magnitude):
+				currentPos["y"] += 1
+				tempCoord = deepcopy(currentPos)
+				coordList.append(tempCoord)
+
+		elif direction == "L":
+			for point in range(0, magnitude):
+				currentPos["x"] -= 1
+				tempCoord = deepcopy(currentPos)
+				coordList.append(tempCoord)
+
+		elif direction == "D":
+			for point in range(0, magnitude):
+				currentPos["y"] -= 1
+				tempCoord = deepcopy(currentPos)
+				coordList.append(tempCoord)
+
+		else:
+			print("You shouldn't be here")
+
+	return coordList
+
+
+
+
+
+coordList1 = directionToCoord(wire1)
+coordList2 = directionToCoord(wire2)
+print(coordList1)
+print(coordList2)
