b327dfe0e4be0399d6efc479c691687bc6a932df
Ryan Aznar <raznar@Ryans-MacBook-Pro-3.local>
2019-12-02 21:17:33 -0500

    Day1 Part 2 solution

--- Day1Part1.py
+++ Day1Part1.py
@@ -13,3 +13,19 @@ for fuel in massArray:
 
 print("Part 1 Total Fuel: " + str(totalFuel))
 
+
+#Part 2
+additionalFuel = []
+with open(part1InputFile, "r") as moduleMass:
+	for mass in moduleMass:
+		tempFuel = int(mass)
+		while (tempFuel > 0):
+			tempFuel = math.floor(tempFuel / 3) - 2
+			if tempFuel > 0:
+				additionalFuel.append(tempFuel)
+
+totalFuel = 0
+for fuel in additionalFuel:
+	totalFuel += fuel
+
+print("Part 2 Total Fuel: " + str(totalFuel))
