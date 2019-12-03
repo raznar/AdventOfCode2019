69789b7bb20f42ad780e205a670a0d84c72e5435
Ryan Aznar <raznar@ncsu.edu>
2019-12-03 11:37:32 -0500

    Initial commit for Day3

---
+++ 3_Day/Day3.py
@@ -0,0 +1,9 @@
+
+
+with open("Day3Input.txt", "r") as input:
+	wire1 = [wire.strip() for wire in input.readline().split(",")]
+	wire2 = [wire.strip() for wire in input.readline().split(",")]
+
+print(wire1)
+print(wire2)
+
