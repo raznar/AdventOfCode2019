47b9ac2158fa392ff0c329a9d6976f5ec4893c2e
Ryan Aznar <raznar@ncsu.edu>
2019-12-05 20:37:08 -0500

    Solved Part 1

--- 3_Day/Segment.py
+++ 3_Day/Segment.py
@@ -1,13 +1,14 @@
 from copy import deepcopy
 
-with open("TestInput.txt", "r") as input:
+with open("Day3Input.txt", "r") as input:
 	wire1 = [wire.strip() for wire in input.readline().split(",")]
 	wire2 = [wire.strip() for wire in input.readline().split(",")]
 
 def generateLineSegs(wire):
-	segList = []
 	originEndPoint = {"x": 0, "y": 0}
 	currentPos = {"x": 0, "y": 0}
+	horSegList = []
+	verSegList = []
 
 	for segment in wire:
 		direction = segment[0]
@@ -16,7 +17,7 @@ def generateLineSegs(wire):
 		if direction == "R":
 			nextCoord = deepcopy(currentPos)
 			nextCoord["x"] += magnitude
-			segList.append({\
+			horSegList.append({\
 				"x1": currentPos["x"],
 				"y1": currentPos["y"],
 				"x2": nextCoord["x"],
@@ -26,7 +27,7 @@ def generateLineSegs(wire):
 		elif direction == "U":
 			nextCoord = deepcopy(currentPos)
 			nextCoord["y"] += magnitude
-			segList.append({\
+			verSegList.append({\
 				"x1": currentPos["x"],
 				"y1": currentPos["y"],
 				"x2": nextCoord["x"],
@@ -36,24 +37,59 @@ def generateLineSegs(wire):
 		elif direction == "L":
 			nextCoord = deepcopy(currentPos)
 			nextCoord["x"] -= magnitude
-			segList.append({\
-				"x1": currentPos["x"],
-				"y1": currentPos["y"],
-				"x2": nextCoord["x"],
-				"y2": nextCoord["y"]})
+			horSegList.append({\
+				"x1": nextCoord["x"],
+				"y1": nextCoord["y"],
+				"x2": currentPos["x"],
+				"y2": currentPos["y"]})
 			currentPos = deepcopy(nextCoord)
 
 		elif direction == "D":
 			nextCoord = deepcopy(currentPos)
 			nextCoord["y"] -= magnitude
-			segList.append({\
-				"x1": currentPos["x"],
-				"y1": currentPos["y"],
-				"x2": nextCoord["x"],
-				"y2": nextCoord["y"]})
+			verSegList.append({\
+				"x1": nextCoord["x"],
+				"y1": nextCoord["y"],
+				"x2": currentPos["x"],
+				"y2": currentPos["y"]})
 			currentPos = deepcopy(nextCoord)
 
-	print(segList)
+	return horSegList, verSegList
 
-print(wire1)
-generateLineSegs(wire1)
+horSegList1, verSegList1 = generateLineSegs(wire1)
+horSegList2, verSegList2 = generateLineSegs(wire2)
+
+def isIntersecting(horSeg, verSeg):
+	x = verSeg["x1"]
+	y = horSeg["y1"]
+
+	if (x > horSeg["x1"] and x < horSeg["x2"]) and \
+		(y > verSeg["y1"] and y < verSeg["y2"]):
+		dist = abs(x) + abs(y)
+		return True, ((x, y), dist)
+	else:
+		return False, ((0, 0), 0)
+
+intersections = []
+
+for horSeg in horSegList1:
+	for verSeg in verSegList2:
+		result = isIntersecting(horSeg, verSeg)
+		if result[0]:
+			intersections.append(result[1])
+
+
+for horSeg in horSegList2:
+	for verSeg in verSegList1:
+		result = isIntersecting(horSeg, verSeg)
+		if result[0]:
+			intersections.append(result[1])
+
+distList = []
+for point in intersections:
+	distList.append(point[1])
+
+distList.sort()
+print("Closest Manhatten Distance is: " + str(distList[0]))
+
+
