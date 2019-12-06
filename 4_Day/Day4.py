87a3b62d6e0ee13fcd0515559a03cae16389309d
Ryan Aznar <raznar@ncsu.edu>
2019-12-05 22:32:06 -0500

    Solved Day 4 Part 2

--- 4_Day/Day4.py
+++ 4_Day/Day4.py
@@ -1,4 +1,3 @@
-password = "206938"
 validPassword = 0
 
 def validatePassword(pw):
@@ -18,3 +17,23 @@ for i in range(206938, 679128):
 		validPassword += 1
 
 print("Number of Valid Passwords from 206938 to 679128: " + str(validPassword))
+
+def validatePasswordP2(pw):
+	for i in range(len(pw) - 1):
+		if pw[i] > pw[i + 1]:
+			return False
+
+	containsDouble = False
+	for i in range(len(pw) - 1):
+		if pw[i] == pw[i + 1]: 
+			if pw.count(pw[i]) == 2:
+				return True
+
+	return containsDouble
+
+validPasswordP2 = 0
+for i in range(206938, 679128):
+	if validatePasswordP2(str(i)):
+		validPasswordP2 += 1
+
+print("P2: Number of Valid Passwords from 206938 to 679128: " + str(validPasswordP2))
